4ac8b4f635ebc8173d9fd8aad63ef3b9845070bc
amruta-khamkar <amrutakhamkar15@gmail.com>
2021-10-03 20:11:10 +0530

    Adding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import List from './Comonents/List';
+import Validations from './Comonents/Validations';
+import Product from './Comonents/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* Task :01 */}
+     <List/>
+
+     {/* Task:02 */}
+     {/* <Validations/> */}
+
+     {/* Task:03 */}
+     {/* <Product/> */}
+    </>
   );
 }
 
